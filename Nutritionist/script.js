7dff11835256ef47f30a0f951fa06d483e070a77
Kevn Cornl <123711097+codekvnnn@users.noreply.github.com>
2023-03-28 22:23:42 -0400

    Update script.js
    adjusted API call
    
    fetch

--- Nutritionist/script.js
+++ Nutritionist/script.js
@@ -1,15 +1,13 @@
-async function {
-    const options = {
-        method: 'GET',
-        headers: {
-            'X-RapidAPI-Key': '38011e3f5bmsh67cf985cd7f0811p11a2a7jsn7da68eae1a14',
-            'X-RapidAPI-Host': 'nutrition-by-api-ninjas.p.rapidapi.com'
-        }
-    };
-    
-    fetch('https://nutrition-by-api-ninjas.p.rapidapi.com/v1/nutrition?query=1lb%20brisket%20with%20fries', options)
-        .then(response => response.json())
-        .then(response => console.log(response))
-        .catch(err => console.error(err));
-        console.log()
-}
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '38011e3f5bmsh67cf985cd7f0811p11a2a7jsn7da68eae1a14',
+		'X-RapidAPI-Host': 'nutrition-by-api-ninjas.p.rapidapi.com'
+	}
+};
+
+console.log(fetch('https://nutrition-by-api-ninjas.p.rapidapi.com/v1/nutrition?query=1lb%20brisket%20with%20fries', options)
+.then(response => response.json())
+.then(response => console.log(response))
+.catch(err => console.error(err)));
+
